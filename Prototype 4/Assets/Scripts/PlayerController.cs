0f5581269d10d0d5ef9a462a71109a60680208ea
Dhruv Pareek <dp00888@srcs.loc>
2020-02-11 15:23:05 -0800

    4.3.4 c

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -31,4 +31,12 @@ public class PlayerController : MonoBehaviour
         }
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Player collided with" + collision.gameObject + " with powerup set to" + hasPowerUp);
+        }
+    }
 }
